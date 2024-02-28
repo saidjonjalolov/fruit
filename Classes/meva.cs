4dcfd8bbe232d89a1d1ad74bd2f068fb0e9516fa
saidjonjalolov <bmwmf8896@gmail.com>
2024-02-28 15:39:14 +0500

    new project 3

---
+++ Classes/meva.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fruit.Classes
+{
+    public class Meva
+    {
+        //field
+        public string name;
+        public string color;
+
+        //property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Color
+        {
+             get
+             {
+                return color;
+             }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Fruit.Classes;
+
+//Call Meva for apple
+Meva apple = new Meva();
+apple.name = "Golden";
+apple.color = "Red";
+//print information
+System.Console.WriteLine($"Apple is name: {apple.Name}\nApple is color: {apple.Color}");
+
+//Call Meva for banana
+Meva banana = new Meva();
+banana.name = "Banana";
+banana.color = "Yellow";
+//print information
+System.Console.WriteLine($"Banana is name: {banana.Name}\nBanana is color: {banana.Color}");
+
+//Call Meva for
+Meva pomegranate = new Meva();
+pomegranate.name = "Pomegranate";
+pomegranate.color = "Red";
+//print information
+System.Console.WriteLine($"Pomegranate is name: {pomegranate.Name}\nPomegranate is color: {pomegranate.Color}");
